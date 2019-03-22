ada2cfc6067f8641b2006f9ad6e052e3ba47deba
ripkurrle <ripkurrle@gmail.com>
2019-03-22 17:11:54 +0000

    initial commit

---
+++ generate.groovy
@@ -0,0 +1,19 @@
+// Simple Groovy script to generate a valid NINO
+
+def startChars = { String alphabet, int i ->
+  new Random().with {
+    (1..i).collect { alphabet[ nextInt( alphabet.length() ) ] }.join()
+  }
+}
+def midNums = { String alphabet, int i ->
+  new Random().with {
+    (1..i).collect { alphabet[ nextInt( alphabet.length() ) ] }.join()
+  }
+}
+def endChar = { String alphabet, int i ->
+  new Random().with {
+    (1..i).collect { alphabet[ nextInt( alphabet.length() ) ] }.join()
+  }
+}
+nino = startChars( ('A'..'Z').join(), 2 ) + midNums( ('0'..'9').join(), 6 ) + endChar( ('A'..'Z').join(), 1 )
+
